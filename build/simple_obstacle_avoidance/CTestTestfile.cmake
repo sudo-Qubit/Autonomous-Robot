9bb613863f6cbb0680414f699704395af66c0d78
Mohamed Oubelaid <m.oubelaid001@gmail.com>
2025-08-09 16:48:04 +0100

    Add RGB and depth cameras

--- build/simple_obstacle_avoidance/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/enryu/Desktop/Path-Following-Robot/src/simple_obstacle_avoidance
-# Build directory: /home/enryu/Desktop/Path-Following-Robot/build/simple_obstacle_avoidance
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/topic_pkg/CMakeFiles/sub_odom_node.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for sub_odom_node.
-# This may be replaced when dependencies are built.
